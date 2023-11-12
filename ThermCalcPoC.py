c993978c468e0251357188c082f83bcfd9551781
ActualGem <149198396+ActualGem@users.noreply.github.com>
2023-11-12 16:20:58 -0500

    Add files via upload
    Proof of concept script. We can rename it and use as reference, but I believe adequate for CDR as of now

---
+++ ThermCalcPoC.py
@@ -0,0 +1,32 @@
+# Constants
+density_copper = 8.96  # g/cm^3
+specific_heat_capacity = float(input("Enter the specific heat capacity in J/g°C: "))
+
+# Rectangular prisms
+masses = []
+for i in range(1, 4):
+    length = float(input(f"Enter the length of rectangular prism {i} in cm: "))
+    width = float(input(f"Enter the width of rectangular prism {i} in cm: "))
+    height = float(input(f"Enter the height of rectangular prism {i} in cm: "))
+    volume = length * width * height
+    mass = volume * density_copper
+    masses.append(mass)
+
+# Cylinder
+radius = float(input("Enter the radius of the cylinder in cm: "))
+height = float(input("Enter the height of the cylinder in cm: "))
+volume = 3.14159 * radius**2 * height
+mass = volume * density_copper
+masses.append(mass)
+
+# Energy
+energy = float(input("Enter the energy in Joules: "))
+
+# Calculate temperature change
+total_mass = sum(masses)
+temperature_change = energy / (total_mass * specific_heat_capacity)
+
+# Convert the temperature change from Celsius to Kelvin
+temperature_change_kelvin = temperature_change + 273.15
+
+print("The change in temperature is", temperature_change_kelvin, "K")
